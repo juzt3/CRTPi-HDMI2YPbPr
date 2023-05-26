5b8d94388fdfc47f65c9f5c0a332ef3b60b278be
juzt3 <juzt3@users.noreply.github.com>
2023-05-26 13:48:16 -0400

    Update setup.sh
    fixed prior errors

--- setup.sh
+++ setup.sh
@@ -4,9 +4,9 @@ sudo apt-get update -y && sudo apt-get install git dialog -y
 git clone https://github.com/juzt3/es-theme-raspixel-320-240-mod
 sudo mv ./es-theme-raspixel-320-240-mod /etc/emulationstation/themes/es-theme-raspixel-320-240-mod
 cd /
-sudo wget https://github.com/crtpi/CRTPi-RGB/raw/master/CRTPi-HDMI2YPbPr.zip
+sudo wget https://github.com/juzt3/CRTPi-HDMI2YPbPr/raw/main/CRTPi-HDMI2YPbPr.zip
 sudo unzip -o -q CRTPi-HDMI2YPbPr.zip
-sudo rm CRTPi-RGB.zip
+sudo rm CRTPi-HDMI2YPbPr.zip
 sudo chmod a+rw -R /opt/retropie/configs/
 sudo chmod a+x -R /opt/retropie/configs/all/*.sh
 sudo reboot
