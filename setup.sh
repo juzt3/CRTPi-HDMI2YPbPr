6335412c51a6e74567af8a46b631923abd2d659a
juzt3 <juzt3@users.noreply.github.com>
2023-06-07 18:42:14 -0400

    flycast from recalbox deleted

--- setup.sh
+++ setup.sh
@@ -8,6 +8,6 @@ sudo wget https://github.com/juzt3/CRTPi-HDMI2YPbPr/raw/main/CRTPi-HDMI2YPbPr.zi
 sudo unzip -o -q CRTPi-HDMI2YPbPr.zip
 sudo rm CRTPi-HDMI2YPbPr.zip
 sudo chmod a+rw -R /opt/retropie/configs/
-sudo chmod a+x -R /opt/retropie/configs/all/*.sh
-sudo chmod a+x /usr/bin/chvideo.py && sudo a+x /usr/bin/consoledisp.py
+sudo chmod 775 -R /opt/retropie/configs/all/*.sh
+sudo chmod 775 /usr/bin/chvideo.py && sudo a+x /usr/bin/consoledisp.py
 sudo reboot
