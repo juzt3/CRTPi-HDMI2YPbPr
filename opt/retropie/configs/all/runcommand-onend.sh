52ef762df2e3e6d9e21cd2dcbe1b2143ad2599d0
juzt3 <juzt3@users.noreply.github.com>
2023-08-19 20:13:04 -0400

    fixed resoulution on end

--- opt/retropie/configs/all/runcommand-onend.sh
+++ opt/retropie/configs/all/runcommand-onend.sh
@@ -1,5 +1,5 @@
-# when exiting an emulator -- revert to 1440x480i
-vcgencmd hdmi_timings 1440 1 110 140 206 472 1 7 4 16 0 0 2 60 1 29862000 1 > /dev/null
+# when exiting an emulator -- revert to 1280x480i
+vcgencmd hdmi_timings 1280 1 37 114 116 480 1 5 4 14 0 0 2 60 1 24365250 1 > /dev/null
 tvservice -e "DMT 6" > /dev/null
 tvservice -e "DMT 87" > /dev/null
 sleep 0.1
